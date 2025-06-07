2428066ac09f860aac7a1c379efb8fe0d19b2cf9
townbox01 <emmanuel_favour@otlook.com>
2025-06-07 13:32:38 +0100

    commit

--- app.py
+++ app.py
@@ -429,4 +429,4 @@ def metrics():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 5000)),debug=True)
+    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 8080)),debug=True)
