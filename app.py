4f3f9e54a609adbab7ea19f6e819d4b52a02b1c5
townbox01 <emmanuel_favour@otlook.com>
2025-06-07 16:04:17 +0100

    commit

--- app.py
+++ app.py
@@ -426,7 +426,7 @@ def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 8080)),debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 8080)),debug=True)
